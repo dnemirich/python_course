29bfb9fd7206caceecb49af775a3d7983a51063a
dnemirich <dnemirich@gmail.com>
2018-09-19 23:40:04 +0300

    done

---
+++ Task1.py
@@ -0,0 +1,19 @@
+
+#This function should return sorted list of distinct elements of e
+def unique(e):
+	e = list(set(e))
+	return(sorted(e))
+
+
+#This function should return transposed dict d. It is garantueed that all the values of dict d are distinct.
+def transposeDict(d):
+  transposed_dict = {key: value for (value, key) in d.items()}
+  print(transposed_dict)
+
+
+#This function should return minimal positive integer which is not present at list e.
+def mex(e):
+  return min([i for i in range(1, len(e)+5) if i not in e])
+
+#This function should return dict with counts of every symbol from string s.
+frequencyDict = lambda s: {el: s.count(el) for el in s}
